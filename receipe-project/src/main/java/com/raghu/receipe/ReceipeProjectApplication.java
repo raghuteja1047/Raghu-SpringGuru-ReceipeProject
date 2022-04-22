410d384c6d54b605a315db88d4d7887b0fea537c
raghu1047 <raghuteja.my07@outlook.com>
2022-04-22 09:59:46 +0530

    Initial Commit

---
+++ receipe-project/src/main/java/com/raghu/receipe/ReceipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.raghu.receipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceipeProjectApplication.class, args);
+	}
+
+}
---
+++ receipe-project/src/test/java/com/raghu/receipe/ReceipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghu.receipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
